caeaa75d3a542ff24bbf697adf8156f65eae88b6
Eric Todd <eterictodd@gmail.com>
2021-10-03 14:35:12 -0600

    Comments associating and validating correctly.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
-class Post < ApplicationRecord
-  validates :title, uniqueness: true, presence: true
+class Comment < ApplicationRecord
   validates :body, presence: true
+  validates :post_id, presence: true
   validates :user_id, presence: true
 
+  belongs_to :post
   belongs_to :user
 end
